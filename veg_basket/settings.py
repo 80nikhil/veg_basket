2b96b60c3fa748fa4db437bcc64681094667191d
80nikhil <nikhilkarole808@gmail.com>
2026-02-11 16:08:03 +0530

    added api changes '

--- veg_basket/settings.py
+++ veg_basket/settings.py
@@ -72,23 +72,23 @@ WSGI_APPLICATION = 'veg_basket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.x/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'veg_basket',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Vegbasket123', 
-#         'HOST': 'database-1.cp6c0ui0qx3h.eu-north-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'veg_basket',
+        'USER': 'postgres',
+        'PASSWORD': 'Vegbasket123', 
+        'HOST': 'database-1.cp6c0ui0qx3h.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
